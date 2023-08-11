197b6ef1bda57b0b32e169ef63031a886d381c9f
Bellhior <cliff_burton85@mail.ru>
2023-08-11 14:21:29 +0400

    fix channel for homework 1

--- src/channel.py
+++ src/channel.py
@@ -12,17 +12,11 @@ class Channel:
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется по id канала. Дальше все данные будут подтягиваться по API."""
         self.__channel_id = channel_id
-        # self.__channel_title = self._snippet['title']
 
     @property
     def channel_id(self) -> str:
         """Возвращает id канала"""
         return self.__channel_id
-
-    @property
-    def channel_title(self) -> str:
-        """Возвращает название канала"""
-        return self.__channel_title
 
     def print_info(self) -> None:
         """Выводит информацию в консоль"""
@@ -39,8 +33,3 @@ class Channel:
         """Выводит информацию в формате JSON"""
         dict_channel = json.dumps(channel, indent=2, ensure_ascii=False)
         print(dict_channel)
-
-    @property
-    def _snippet(self) -> dict:
-        return self.get_info
-
